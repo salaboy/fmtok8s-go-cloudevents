0d15a7a2d8f392fe05754b7e733b0f4440cce25e
salaboy <Salaboy@gmail.com>
2022-01-06 09:19:15 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# fmtok8s-go-cloudevents
-Simple example in Go using CloudEvents SDK
+# Simple CloudEvents Application using Go and Gorilla MUX
+Simple example in Go using CloudEvents SDK. This example exposes two HTTP REST endpoints, a producer and a consumer of CloudEvents. 
+
+- `/produce` accepts a POST request and produce a CloudEvent to the `SINK` that by default is `http://localhost:8080/`
+
+This example uses Gorilla MUX to handle different requests to different endpoints. 
+
+The application by default runs on port 8081.
+
+
+## Building and running the application
+
+If you want to build from source and run it you need to have Go installed. 
+
+To simple run the application: 
+
+```
+go run main.go
+```
+
+You can use Google `ko` to build an image and push a docker image, if you have `ko` installed you can run:
+
+```
+ko publish main.go
+```
+
+To build, publish and run a docker image with `ko`:
+
+```
+docker run -p 8081:8081 $(ko publish main.go)
+```
+
+Or if you just want to use Docker:
+
+```
+docker run -e SINK=http://localhost:8080 -p 8081:8081 salaboy/fmtok8s-go-cloudevents.go
+```
+
+## Sending Requests and CloudEvents
+
+Send a POST request to produce a CloudEvent, that will be sent to a downstream service. 
+
+```
+curl -X POST localhost:8081/produce -v
+```
+
+Consume a CloudEvent and print it out: 
+```
+curl -X POST http://localhost:8081/ -H "Content-Type: application/json" -H "ce-type: MyCloudEvent"  -H "ce-id: 123"  -H "ce-specversion: 1.0" -H "ce-source: curl-command" -d '{"myData" : "hello from curl", "myCounter" : 1 }'
+```
---
+++ fmtok8s-go-cloudevents.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+)
+
+var SINK = getEnv("SINK", "http://localhost:8081")
+
+var PORT = getEnv("SERVER_PORT", ":8081")
+
+type MyCloudEventData struct{
+	MyData    string `json:"myData"`
+	MyCounter int `json:"myCounter"`
+
+}
+
+func ConsumeCloudEventHandler(ctx context.Context, event cloudevents.Event) {
+	data := MyCloudEventData{}
+	fmt.Printf("Got an Event: %s", event)
+	json.Unmarshal(event.Data(),&data)
+
+	fmt.Printf("MyCloudEventData Data: %v\n", data.MyData)
+	fmt.Printf("MyCloudEventData Counter: %v\n", data.MyCounter)
+}
+
+func ProduceCloudEventHandler(writer http.ResponseWriter, request *http.Request) {
+	p, err := cloudevents.NewHTTP()
+	if err != nil {
+		log.Fatalf("failed to create protocol: %s", err.Error())
+	}
+
+	c, err := cloudevents.NewClient(p, cloudevents.WithTimeNow(), cloudevents.WithUUIDs())
+	if err != nil {
+		log.Fatalf("failed to create client, %v", err)
+	}
+
+	// Create an Event.
+	event := cloudevents.NewEvent()
+	event.SetSource("application-b")
+	event.SetType("MyCloudEvent")
+	data := MyCloudEventData{}
+	data.MyData = "hello from Go"
+	data.MyCounter = 1
+	event.SetData(cloudevents.ApplicationJSON, &data)
+
+	// Set a target.
+	ctx := cloudevents.ContextWithTarget(context.Background(), SINK)
+	// Send that Event.
+
+
+	if result := c.Send(ctx, event); !cloudevents.IsACK(result) {
+		log.Fatalf("failed to send (!IsACK), %v", result)
+	}
+
+	respondWithJSON(writer, http.StatusOK, "OK")
+}
+
+func main() {
+
+	ctx := context.Background()
+	p, err := cloudevents.NewHTTP()
+	if err != nil {
+		log.Fatalf("failed to create protocol: %s", err.Error())
+	}
+
+	h, err := cloudevents.NewHTTPReceiveHandler(ctx, p, ConsumeCloudEventHandler)
+	if err != nil {
+		log.Fatalf("failed to create handler: %s", err.Error())
+	}
+
+	// Use a gorilla mux implementation for the overall http handler.
+	router := mux.NewRouter()
+
+	router.HandleFunc("/produce", ProduceCloudEventHandler).Methods("POST")
+
+	router.Handle("/", h)
+
+	log.Printf("will listen on %v\n", PORT)
+	if err := http.ListenAndServe(PORT, router); err != nil {
+		log.Fatalf("unable to start http server, %s", err)
+	}
+
+
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return defaultValue
+	}
+	return value
+}
